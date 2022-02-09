e62305e291c1f7bba15a9dc273e7fe0954c65112
den14-dn <den14-dn@yandex.ru>
2022-02-09 18:27:58 +0300

    2.2. Модель данных - Post. [#285211 #262463]

--- src/main/java/ru/job4j/grabber/Post.java
+++ src/main/java/ru/job4j/grabber/Post.java
@@ -76,11 +76,15 @@ public class Post {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
         Post post = (Post) o;
-        return id == post.id &&
-                Objects.equals(link, post.link);
+        return id == post.id
+                && Objects.equals(link, post.link);
     }
 
     @Override

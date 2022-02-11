21e35a694a5acbb00cdfbcaa1f34f85ffcbbdc0d
den14-dn <den14-dn@yandex.ru>
2022-02-12 01:53:19 +0300

    2.4. SqlRuParse [#285213]

--- src/main/java/ru/job4j/html/SqlRuParse.java
+++ src/main/java/ru/job4j/html/SqlRuParse.java
@@ -4,46 +4,54 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
+import ru.job4j.grabber.Parse;
 import ru.job4j.grabber.Post;
+import ru.job4j.grabber.utils.DateTimeParser;
 import ru.job4j.grabber.utils.SqlRuDateTimeParser;
 
 import java.io.IOException;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
-public class SqlRuParse {
+public class SqlRuParse implements Parse {
     private static final int COUNT_PAGES = 5;
     private static final String URL_SITE = "https://www.sql.ru/forum/job-offers/";
+    private final DateTimeParser dateTimeParser;
 
-    public static void main(String[] args) throws Exception {
-        SqlRuParse parser = new SqlRuParse();
-        for (int pageNumber = 1; pageNumber <= parser.COUNT_PAGES; pageNumber++) {
-            parser.parseURL(parser.URL_SITE + pageNumber);
-        }
+    public SqlRuParse(DateTimeParser dateTimeParser) {
+        this.dateTimeParser = dateTimeParser;
     }
 
-    private void parseURL(String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
-        Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
-            Element parent = td.parent();
-            LocalDateTime created = new SqlRuDateTimeParser().parse(parent.child(5).text());
-            Element href = td.child(0);
-            String link = href.attr("href");
-            System.out.printf("%s - %s - '%s'", created, link, href.text());
-            System.out.println(System.lineSeparator());
-
-            Post post = getDataPost(link);
-            System.out.println(post.toString());
-            System.out.println(System.lineSeparator());
-        }
+    public static void main(String[] args) {
+        SqlRuParse parse = new SqlRuParse(new SqlRuDateTimeParser());
+        List<Post> list = parse.list(URL_SITE);
     }
 
-    private Post getDataPost(String url) throws IOException {
-        Document doc = Jsoup.connect(url).get();
+    @Override
+    public List<Post> list(String link) {
+        List<Post> list = new ArrayList<>();
+        for (int pageNumber = 1; pageNumber <= COUNT_PAGES; pageNumber++) {
+            try {
+                Document doc = Jsoup.connect(link + pageNumber).get();
+                Elements row = doc.select(".postslisttopic");
+                for (Element td : row) {
+                    list.add(detail(td.child(0).attr("href")));
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return list;
+    }
+
+    @Override
+    public Post detail(String link) throws IOException {
+        Document doc = Jsoup.connect(link).get();
         String title = doc.title();
         String stringDate = doc.select(".msgFooter").get(0).text();
         LocalDateTime created = new SqlRuDateTimeParser().parse(stringDate.substring(0, stringDate.indexOf('[')));
         String description = doc.select(".msgBody").get(1).text();
-        return new Post(title, url, description, created);
+        return new Post(title, link, description, created);
     }
 }
